cd84504769ac02879690ec56983fa82c2077e181
Javier Román <jromanmorales96@gmail.com>
2019-06-03 13:07:00 +0200

    Solving some issues

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +x newproject.sh
-cp newproject.sh /usr/bin
-cp create_remote_project.py /usr/bin
+chmod +x newproject
+cp newproject /usr/bin
+cp create_remote_repo.py /usr/bin
 cp access_token.json /usr/bin
